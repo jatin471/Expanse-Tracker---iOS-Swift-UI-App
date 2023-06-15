70f25df775913da7bf712d96be4fc1197f770457
Jatin Yadav <jatinyadav471@gmail.com>
2023-06-15 14:54:27 +0530

    Initial Commit

---
+++ ExpenseTrack/ExpenseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackApp.swift
+//  ExpenseTrack
+//
+//  Created by JATIN YADAV on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
